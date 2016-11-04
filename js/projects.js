c65bf6b72bf012f279279a301efcef7174b28d46
Unknown <nullruto@protonmail.ch>
2016-11-04 14:35:57 -0400

    Added table to projects.html

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,15 @@
+//http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format/4673436#4673436
+if (!String.format) {
+    String.format = function(format) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return format.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ?
+                args[number] :
+                match;
+        });
+    };
+}
+
 window.onload = function() {
     var url = 'https://api.github.com/users/nullruto/repos';
     $.getJSON(url, function(data) {
@@ -7,7 +19,7 @@ window.onload = function() {
             var language = val["language"];
             var pushdate = val["pushed_at"];
             var creationdate = val["created_at"];
-            $(".content").append("<br/>" + val["name"] + ": " + val["git_url"] + "<br/>");
+            $(".gitbody").append(String.format("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>", name, language, giturl));
         });
     });
 }
