f71e87375c545800f67f406d6b64288393f48b01
Unknown <nullruto@protonmail.ch>
2016-11-04 15:01:09 -0400

    Added more repos to show in projects.html

--- js/projects.js
+++ js/projects.js
@@ -19,7 +19,29 @@ window.onload = function() {
             var language = val["language"];
             var pushdate = val["pushed_at"];
             var creationdate = val["created_at"];
-            $(".gitbody").append(String.format("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>", name, language, giturl));
+            $(".nullrutobody").append(String.format("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>", name, language, giturl));
+        });
+    });
+    url = 'https://api.github.com/users/rut0/repos';
+    $.getJSON(url, function(data) {
+        $.each(data, function(key, val) {
+            var name = val["name"];
+            var giturl = val["git_url"];
+            var language = val["language"];
+            var pushdate = val["pushed_at"];
+            var creationdate = val["created_at"];
+            $(".rut0body").append(String.format("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>", name, language, giturl));
+        });
+    });
+    url = 'https://api.github.com/users/nullruto/starred';
+    $.getJSON(url, function(data) {
+        $.each(data, function(key, val) {
+            var name = val["name"];
+            var giturl = val["git_url"];
+            var language = val["language"];
+            var pushdate = val["pushed_at"];
+            var creationdate = val["created_at"];
+            $(".starredbody").append(String.format("<tr><td>{0}</td><td>{1}</td><td>{2}</td></tr>", name, language, giturl));
         });
     });
 }
